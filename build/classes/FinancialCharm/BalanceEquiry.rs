57a472eab963801c54c3baf5e532fc9cb22d9c94
Saif Mohammad Khan <82087970+froyo-saif@users.noreply.github.com>
2022-01-10 19:23:12 +0530

    Add files via upload

---
+++ build/classes/FinancialCharm/BalanceEquiry.rs
@@ -0,0 +1,2 @@
+ASimulatorSystem.BalanceEnquiry
+FinancialCharm.BalanceEnquiry
---
+++ src/FinancialCharm/Conn.java
@@ -0,0 +1,20 @@
+package FinancialCharm;
+
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///fcb","root","");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
